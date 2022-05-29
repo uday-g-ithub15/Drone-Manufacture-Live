4958e747854a4b7db9d760fecd32316c7948a9e2
uday-g-ithub15 <mudayhasan@gmail.com>
2022-05-29 19:40:38 +0600

    Edit Readme File

--- src/pages/Shared/Header/Navbar.js
+++ src/pages/Shared/Header/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link to={'/home'}>Home</Link></li>
+                        <li><Link to={'/'}>Home</Link></li>
                         <li><Link to={'/parts'}>Parts</Link></li>
                         <li><Link to={'/dashboard'}>Dashboard</Link></li>
                         <li><Link to={'/blogs'}>Blogs</Link></li>
@@ -34,7 +34,7 @@ const Navbar = () => {
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
-                    <li className=' hover:bg-primary transition-all duration-200'><Link to={'/home'}>Home</Link></li>
+                    <li className=' hover:bg-primary transition-all duration-200'><Link to={'/'}>Home</Link></li>
                     <li className=' hover:bg-primary transition-all duration-200'><Link to={'/parts'}>Parts</Link></li>
                     <li className=' hover:bg-primary transition-all duration-200'><Link to={'/dashboard'}>Dashboard</Link></li>
                     <li className=' hover:bg-primary transition-all duration-200'><Link to={'/portfolio'}>My Portfolio</Link></li>
