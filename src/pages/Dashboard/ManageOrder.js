3002ef6492f578d90736eb49c1c24cbab6fb5125
uday-g-ithub15 <mudayhasan@gmail.com>
2022-05-29 21:26:20 +0600

    Delete Part

--- src/pages/Dashboard/ManageOrder.js
+++ src/pages/Dashboard/ManageOrder.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import useParts from '../../hooks/useParts';
+import Loading from '../Shared/Loading';
 
 const ManageOrder = () => {
+    const { data, isLoading } = useParts(`http://localhost:5000/allorders`)
+    // const { data, isLoading } = useParts(`https://secret-everglades-45349.herokuapp.com/orders`)
+    if (isLoading) {
+        return <Loading />
+    }
     return (
         <div>
-            <h1>Manage Order</h1>
+            <h1>Manage Order : {data.length} </h1>
         </div>
     );
 };
--- src/pages/Dashboard/ManageProduct.js
+++ src/pages/Dashboard/ManageProduct.js
@@ -1,10 +1,52 @@
-import React from 'react';
+// import React, { useState } from 'react';
+import useParts from '../../hooks/useParts';
+import Loading from '../Shared/Loading';
 
 const ManageProduct = () => {
+    const { data: orders, isLoading, refetch } = useParts(`https://secret-everglades-45349.herokuapp.com/parts`)
+    if (isLoading) {
+        return <Loading />
+    }
+    const handleCancel = id => {
+        fetch(`https://secret-everglades-45349.herokuapp.com/part/${id}`, {
+            method: "DELETE"
+        }).then(res => res.json()).then(data => {
+            refetch()
+        })
+    }
     return (
-        <div>
-            <h1>Manage Product</h1>
-        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th  ></th>
+                    <th  >Name</th>
+                    <th  >Status</th>
+                    <th  >Cancelation</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    orders.map((order, index) => <tr key={order._id}>
+                        <td  >{index + 1}</td>
+                        <td  >{order.name}</td>
+                        <td  >UnPaid</td>
+                        <td  ><label htmlFor=" confirm-modal" className='btn btn-sm bg-stone-700 text-slate-200 font-semibold'>Cancel Order</label>
+
+                        </td>
+                        <input type="checkbox" id=" confirm-modal" class="modal-toggle" />
+                        <div class="modal modal-bottom sm:modal-middle">
+                            <div class="modal-box">
+                                <h3 class="font-bold text-lg">Are you sure you want to cancel this order ? </h3>
+                                <div class="modal-action">
+                                    <label htmlFor=" confirm-modal" class="btn btn-primary text-white font-bold" onClick={() => handleCancel(order._id)}>Yes</label>
+                                    <label htmlFor=" confirm-modal" class="btn btn-primary text-white font-bold">Not now</label>
+                                </div>
+                            </div>
+                        </div>
+                    </tr>)
+                }
+            </tbody>
+        </table>
     );
 };
 
